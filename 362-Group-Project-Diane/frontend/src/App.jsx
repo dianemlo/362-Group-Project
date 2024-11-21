d69228f863d9c99f3148b86509e18146ca26f147
JasonGuanCpp <jasonguan79@gmail.com>
2024-11-20 21:04:33 -0800

    create auth context

--- 362-Group-Project-Diane/frontend/src/App.jsx
+++ 362-Group-Project-Diane/frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/Home'
 import { Toaster } from 'react-hot-toast'
+import { useAuthContext } from "./context/AuthContext"
 
 
 // to show the Login page
@@ -28,12 +29,13 @@ import { Toaster } from 'react-hot-toast'
 
 // to show the Home page
 function App() {
+  const {authUser}= useAuthContext();
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <Routes>
-        <Route path='/' element={<Home /> } /> 
-        <Route path='/login' element={<Login /> } /> 
-        <Route path='/signup' element={<SignUp /> } /> 
+        <Route path='/' element={authUser ? <Home /> : <Navigate to ={"/login"} />} /> 
+        <Route path='/login' element={authUser ? <Navigate to ="/" /> : <Login />} /> 
+        <Route path='/signup' element={authUser ? <Navigate to ="/" /> : <SignUp />} /> 
       </Routes>
       <Toaster />
     </div>
